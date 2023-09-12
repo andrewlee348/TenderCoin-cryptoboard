b68b0621b355b3eabfd70701b4cb3aa43ef613e9
a348lee <a348lee@uwaterloo.ca>
2023-09-12 02:12:39 -0400

    Fully implemented Sidebar and Header for pages

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/topbar.jsx";
-// import Sidebar from "./scenes/global/sidebar.jsx";
-// import Dashboard from "./scenes/dashboard";
-// import Portfolio from "./scenes/dashboard";
-// import Bitcoin from "./scenes/dashboard";
-// import Etherium from "./scenes/dashboard";
-// import Dashboard from "./scenes/dashboard";
-// import Dashboard from "./scenes/dashboard";
+import Sidebar from "./scenes/global/sidebar.jsx";
+import Dashboard from "./scenes/dashboard";
+// import Portfolio from "./scenes/portfolio";
+// import MostPopular from "./scenes/mostpopular";
+// import TopGainers from "./scenes/topgainers";
+// import BigDippers from "./scenes/bigdippers";
+// import Feedback from "./scenes/feedback";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -18,8 +18,17 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/portfolio" element={<Portfolio />} /> */}
+              {/* <Route path="/most_popular" element={<MostPopular />} /> */}
+              {/* <Route path="/top_gainers" element={<TopGainers />} /> */}
+              {/* <Route path="/big_dippers" element={<BigDippers />} /> */}
+              {/* <Route path="/feedback" element={<Feedback />} /> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
--- src/scenes/global/sidebar.jsx
+++ src/scenes/global/sidebar.jsx
@@ -1,5 +1,177 @@
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import ArticleIcon from "@mui/icons-material/Article";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import TrendingDownIcon from "@mui/icons-material/TrendingDown";
+import InsertCommentIcon from "@mui/icons-material/InsertComment";
+
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <MenuItem
+      active={selected === title}
+      style={{ color: colors.grey[100] }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+    >
+      <Typography>{title}</Typography>
+      <Link to={to} />
+    </MenuItem>
+  );
+};
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+
+  // const sidebarWidth = isCollapsed ? "64px" : "250px";
+
+  return (
+    <Box
+      sx={{
+        // width: sidebarWidth,
+        // transition: "width 0.2s ease-in-out",
+        // overflowX: "hidden",
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: "transparent !important",
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important",
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important",
+        },
+        "& .pro-menu-item.active": {
+          color: "6870fa !important",
+        },
+      }}
+    >
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape="square">
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0px 20px 0px",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Typography variant="h3" color={colors.grey[100]}>
+                  Crypto-Board
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+
+          {/* USER */}
+          {!isCollapsed && (
+            <Box mb="25px">
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <img
+                  alt="profile-user"
+                  width="100px"
+                  height="100px"
+                  onScroll={"../../assets/user.png"}
+                  style={{ cursor: "pointer", borderRadius: "50%" }}
+                />
+              </Box>
+
+              <Box textAlign="center">
+                <Typography
+                  variant="h2"
+                  color={colors.grey[100]}
+                  fontWeight="bold"
+                  sx={{ m: "10px 0 0 0" }}
+                >
+                  Andrew Lee
+                </Typography>
+                <Typography variant="h5" color={colors.greenAccent[500]}>
+                  STUFF
+                </Typography>
+              </Box>
+            </Box>
+          )}
+
+          {/* MENU ITEMS */}
+          <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+            <Item
+              title="Dashboard"
+              to="/"
+              icon={<HomeOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Portfolio"
+              to="/portfolio"
+              icon={<ArticleIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Feedback"
+              to="/feedback"
+              icon={<InsertCommentIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0px 5px 20px" }}
+            >
+              Data
+            </Typography>
+            <Item
+              title="Most Popular"
+              to="/most_popular"
+              icon={<WhatshotIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Top Gainers"
+              to="/top_gainers"
+              icon={<TrendingUpIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Big Dippers"
+              to="/big_dippers"
+              icon={<TrendingDownIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          </Box>
+        </Menu>
+      </ProSidebar>
+    </Box>
+  );
 };
 
 export default Sidebar;
