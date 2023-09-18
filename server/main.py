64e96db17c3601690945fd74902635b42c595c88
Andrew Lee <andrewlee@andrews-air-2.home>
2023-09-18 15:48:27 -0400

    AppEngine deployment save

--- server/main.py
+++ server/main.py
@@ -5,30 +5,6 @@ import krakenex
 import requests
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
-import functions_framework
-
-# @functions_framework.http
-# def hello_http(request):
-#     """HTTP Cloud Function.
-#     Args:
-#         request (flask.Request): The request object.
-#         <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-#     Returns:
-#         The response text, or any set of values that can be turned into a
-#         Response object using `make_response`
-#         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-#     """
-#     request_json = request.get_json(silent=True)
-#     request_args = request.args
-
-#     if request_json and 'name' in request_json:
-#         name = request_json['name']
-#     elif request_args and 'name' in request_args:
-#         name = request_args['name']
-#     else:
-#         name = 'World'
-#     return 'Hell {}!'.format(name)
-
 
 app = Flask(__name__)
 CORS(app)
